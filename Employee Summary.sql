1e8a2848d5571a111950c9be8772a6794ecc08c2
mdas77hx <66260219+mdas77hx@users.noreply.github.com>
2020-06-02 23:36:40 +0300

    Exercise 10 from Subqueries and JOINs

---
+++ Employee Summary.sql
@@ -0,0 +1,13 @@
+use soft_uni;
+
+select e.employee_id, 
+concat(e.first_name, ' ', e.last_name) as employee_name,
+concat(m.first_name, ' ', m.last_name) as manager_name,
+d.`name` as department_name		 
+from employees as e
+join employees as m
+on e.manager_id = m.employee_id
+join departments as d
+on e.department_id = d.department_id
+order by e.employee_id
+limit 5;
